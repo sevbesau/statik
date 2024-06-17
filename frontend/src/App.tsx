bf31ad183d00f299fefd0a687b3188e387e808e2
sevbesau <seppe@siliconminds.be>
2024-06-17 16:59:11 +0200

    factored out the reservation form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,67 +3,18 @@ import { VisitorForm } from "./components/app/VisitorForm";
 import { Visitor } from "./schemas/visitor.schema";
 import VisitorCard from "./components/app/VisitorCard";
 
-import {
-  Select,
-  SelectTrigger,
-  SelectContent,
-  SelectItem,
-  SelectValue,
-} from "./components/ui/select";
-import { useForm } from "react-hook-form";
 import { Reservation, reservationSchema } from "./schemas/reservation.schema";
-import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "./components/ui/form";
-import { Button } from "./components/ui/button";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "./components/ui/popover";
-import { format } from "date-fns";
-import { cn } from "./lib/utils";
-import { CalendarIcon } from "lucide-react";
-import { Calendar } from "./components/ui/calendar";
-import { nlBE } from "date-fns/locale/nl-BE";
+
 import { createReservation } from "./lib/api";
 import { Toaster } from "./components/ui/toaster";
 import { useToast } from "./components/ui/use-toast";
-
-const ZOO_OPENING_TIME_H = 10;
-const ZOO_CLOSING_TIME_H = 20;
-const TIMESLOT_DURATION_H = 2;
-
-const timeslots: string[] = [];
-
-for (
-  let timeslotStart = ZOO_OPENING_TIME_H;
-  timeslotStart < ZOO_CLOSING_TIME_H - TIMESLOT_DURATION_H;
-  timeslotStart += TIMESLOT_DURATION_H
-) {
-  timeslots.push(
-    `${timeslotStart}:00 - ${timeslotStart + TIMESLOT_DURATION_H}:00`
-  );
-}
+import ReservationForm from "./components/app/ReservationFrom";
 
 function App() {
   const { toast } = useToast();
   const [visitors, setVisitors] = useState<Visitor[]>([]);
 
-  const form = useForm<Reservation>({
-    resolver: zodResolver(reservationSchema),
-    defaultValues: {
-      date: new Date(),
-    },
-  });
-
-  async function onSubmit(reservation: Reservation) {
+  async function submitReservation(reservation: Reservation) {
     if (!visitors || visitors.length <= 0) {
       toast({
         title: "Geen bezoekers",
@@ -99,7 +50,6 @@ function App() {
       </h1>
 
       <h2 className="text-xl">Bezoekers:</h2>
-
       <VisitorForm onSubmit={addVisitor} />
       <ul className="grid grid-cols-2 gap-4">
         {visitors.map((visitor, visitorIndex) => (
@@ -117,85 +67,8 @@ function App() {
       </ul>
 
       <h2 className="text-xl">Reservatie details:</h2>
+      <ReservationForm onSubmit={submitReservation} />
 
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="w-2/3 space-y-6"
-        >
-          <FormField
-            control={form.control}
-            name="date"
-            render={({ field }) => (
-              <FormItem className="flex flex-col space-y-3">
-                <FormLabel>Datum</FormLabel>
-                <Popover>
-                  <PopoverTrigger asChild>
-                    <FormControl>
-                      <Button
-                        variant={"outline"}
-                        className={cn(
-                          "w-[240px] pl-3 text-left font-normal",
-                          !field.value && "text-muted-foreground"
-                        )}
-                      >
-                        {field.value ? (
-                          format(field.value, "PPP")
-                        ) : (
-                          <span>Kies een datum</span>
-                        )}
-                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                      </Button>
-                    </FormControl>
-                  </PopoverTrigger>
-                  <PopoverContent className="w-auto p-0" align="start">
-                    <Calendar
-                      mode="single"
-                      selected={field.value}
-                      onSelect={field.onChange}
-                      disabled={[
-                        (date) => date < new Date(),
-                        { dayOfWeek: [1, 2] },
-                      ]}
-                      initialFocus
-                      locale={nlBE}
-                    />
-                  </PopoverContent>
-                </Popover>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="timeslot"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Tijdslot</FormLabel>
-                <Select
-                  onValueChange={field.onChange}
-                  defaultValue={field.value}
-                >
-                  <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Kies een tijdslot" />
-                    </SelectTrigger>
-                  </FormControl>
-                  <SelectContent>
-                    {timeslots.map((timeslot) => (
-                      <SelectItem key={timeslot} value={timeslot}>
-                        {timeslot}
-                      </SelectItem>
-                    ))}
-                  </SelectContent>
-                </Select>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit">Maak reservatie!</Button>
-        </form>
-      </Form>
       <Toaster />
     </main>
   );
---
+++ frontend/src/components/app/ReservationFrom.tsx
@@ -0,0 +1,130 @@
+import { cn } from "@/lib/utils";
+import { Button } from "../ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { Calendar } from "../ui/calendar";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+
+import { nlBE } from "date-fns/locale/nl-BE";
+import { useForm, useFormContext } from "react-hook-form";
+import { Reservation, reservationSchema } from "@/schemas/reservation.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const ZOO_OPENING_TIME_H = 10;
+const ZOO_CLOSING_TIME_H = 20;
+const TIMESLOT_DURATION_H = 2;
+
+const timeslots: string[] = [];
+
+for (
+  let timeslotStart = ZOO_OPENING_TIME_H;
+  timeslotStart < ZOO_CLOSING_TIME_H - TIMESLOT_DURATION_H;
+  timeslotStart += TIMESLOT_DURATION_H
+) {
+  timeslots.push(
+    `${timeslotStart}:00 - ${timeslotStart + TIMESLOT_DURATION_H}:00`
+  );
+}
+
+interface ReservationFormProps {
+  onSubmit: (reservation: Reservation) => void;
+}
+
+export default function ReservationForm({ onSubmit }: ReservationFormProps) {
+  const form = useForm<Reservation>({
+    resolver: zodResolver(reservationSchema),
+    defaultValues: {
+      date: new Date(),
+    },
+  });
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+        <FormField
+          control={form.control}
+          name="date"
+          render={({ field }) => (
+            <FormItem className="flex flex-col space-y-3">
+              <FormLabel>Datum</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[240px] pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Kies een datum</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    disabled={[
+                      (date) => date < new Date(),
+                      { dayOfWeek: [1, 2] },
+                    ]}
+                    initialFocus
+                    locale={nlBE}
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="timeslot"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Tijdslot</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Kies een tijdslot" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {timeslots.map((timeslot) => (
+                    <SelectItem key={timeslot} value={timeslot}>
+                      {timeslot}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Maak reservatie!</Button>
+      </form>
+    </Form>
+  );
+}
