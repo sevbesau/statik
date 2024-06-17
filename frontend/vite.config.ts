06c09f41313ab8032711e087aa354922cc9a10f5
sevbesau <seppe@siliconminds.be>
2024-06-17 14:04:25 +0200

    setup frontend - vite + react + ts + shadcnui

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,13 @@
-import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
