c37e461e54b50ca748b9f34997bc77da9ba8c512
sevbesau <seppe@siliconminds.be>
2024-06-17 12:38:54 +0200

    cleanup unused code

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ReservationsModule } from './reservations/reservations.module';
 import { PrismaService } from './lib/prisma/prisma.service';
 import { PrismaModule } from './lib/prisma/prisma.module';
 
 @Module({
   imports: [ReservationsModule, PrismaModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [PrismaService],
   exports: [PrismaService],
 })
 export class AppModule {}
